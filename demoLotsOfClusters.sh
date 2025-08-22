a3847ab61f1a3208475b926af00570380718b952
timeless <timelessness@gmail>
2025-08-22 11:57:26 -0400

    demo updates

--- demoLotsOfClusters.sh
+++ demoLotsOfClusters.sh
@@ -8,9 +8,9 @@ echo " - Setting up a buncha Clusters!"
 sleep 5
 
 # set up 6 clusters
-sleep 2 ; adviser  cluster create  --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 8 ; adviser  cluster create  --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser'
+sleep 2 ; adviser  cluster create  --short-name anni     --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
+sleep 2 ; adviser  cluster create  --short-name brynne   --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
+sleep 2 ; adviser  cluster create  --short-name cephisto --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
+sleep 2 ; adviser  cluster create  --short-name darren   --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
+sleep 2 ; adviser  cluster create  --short-name ellis    --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
+sleep 8 ; adviser  cluster create  --short-name fez      --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser'
--- demoLotsOfJobs.sh
+++ demoLotsOfJobs.sh
@@ -5,13 +5,13 @@ echo " adviser status --watch 1"
 echo ""
 echo " - Running a buncha Jobs!"
 
-sleep 5
+sleep 3
 
 # run a buncha jobs
-sleep 5 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
-sleep 6 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
-sleep 7 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
-sleep 8 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
-sleep 7 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
-sleep 6 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
-sleep 5 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 1 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 2 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 3 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 4 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 3 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 2 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
+sleep 1 ; adviser run  "bash ./demoAdviserCommandProgress.sh && ls -al" &
