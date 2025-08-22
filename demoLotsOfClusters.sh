6be2278df0f88d4742651bfa05f9542a2997002e
timeless <timelessness@gmail>
2025-08-22 17:41:02 -0400

    many clusters named

--- demoLotsOfClusters.sh
+++ demoLotsOfClusters.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+set -e
+
+numClusters=${1-6}
+# Array of cluster short names
+short_names=(advistar tim david krish brian annya bethany christy denise erica fez grant harold ilya jefferson kevin lenny manny nandy olyvya pandies quendan rita sia tandy uvmap vertex wyn xander ze)
+
+# Check if requested number exceeds available names
+if [ $numClusters -gt ${#short_names[@]} ]; then
+    echo "Error: Requested $numClusters clusters, but only ${#short_names[@]} short names are available."
+    echo "Maximum supported clusters: ${#short_names[@]}"
+    exit 1
+fi
+
 echo " - Before doing this, go do this in another terminal:"
 echo " adviser status --watch 1"
 echo ""
-echo " - Setting up a buncha Clusters!"
+echo " - Setting up $numClusters Clusters! (You can specify a different number if you want)"
+echo " - Like so: \"$0 10\" for 10."
+echo ""
 
-sleep 5
+sleep 3
 
-# set up 6 clusters
-sleep 2 ; adviser  cluster create  --short-name anni     --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --short-name brynne   --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --short-name cephisto --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --short-name darren   --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 2 ; adviser  cluster create  --short-name ellis    --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser' &
-sleep 8 ; adviser  cluster create  --short-name fez      --cloud aws --instance-type t2.medium  --workdir . --region us-west-2  --setup 'echo Adviser'
+# Use only the first numClusters names from the array
+clusters_to_create=("${short_names[@]:0:$numClusters}")
+
+# Create clusters in a loop
+for i in "${!clusters_to_create[@]}"; do
+    short_name="${clusters_to_create[$i]}"
+
+    if [ $i -eq $((${#clusters_to_create[@]} - 1)) ]; then
+        # Last cluster - don't background it
+        sleep 5
+        adviser cluster create --short-name "$short_name" --cloud aws --instance-type t2.medium --workdir . --region us-west-2 --setup 'echo Adviser'
+    else
+        # Background all other clusters
+        sleep 0.5
+        adviser cluster create --short-name "$short_name" --cloud aws --instance-type t2.medium --workdir . --region us-west-2 --setup 'echo Adviser' &
+    fi
+done
